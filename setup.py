581aeec960e5cfc1221807bffbf03328f81db67b
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-11 18:45:18 +0100

    fixed bug where debug doesnt take self

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylogger',
-    version='1.0.1',
+    version='1.0.2',
     python_requires='>=3.6, <4.0',
     packages=find_packages(),
     install_requires=[
